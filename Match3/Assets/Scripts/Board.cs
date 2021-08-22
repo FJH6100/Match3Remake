499ecc4ef04799b61dfd936bf615e2ac9ffc8f41
FJH6100 <39116989+FJH6100@users.noreply.github.com>
2021-08-22 00:41:50 -0500

    Finding Matches
    I can now match gems and respawn gems as they're destroyed.

--- Match3/Assets/Scripts/Board.cs
+++ Match3/Assets/Scripts/Board.cs
@@ -8,11 +8,20 @@ public class Board : MonoBehaviour
     public int height;
     public GameObject backgroundTile;
     public Gem[] gems;
-    Gem[,] gemLayout;
+    [HideInInspector]
+    public Gem[,] allGems;
+    public float gemSpeed;
+    [HideInInspector]
+    public MatchFinder matchFind;
+
+    private void Awake()
+    {
+        matchFind = FindObjectOfType<MatchFinder>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        gemLayout = new Gem[width,height];
+        allGems = new Gem[width,height];
         Setup();
     }
 
@@ -27,17 +36,100 @@ public class Board : MonoBehaviour
                 bg.transform.parent = transform;
                 bg.name = "Tile " + i + " " + j;
                 int gemIndex = Random.Range(0, gems.Length);
-                Gem gemToUse = gems[gemIndex];
-                SpawnGem(gemToUse, i, j);
+                while (MatchesAt(new Vector2Int(i,j), gems[gemIndex]))
+                {
+                    gemIndex = Random.Range(0, gems.Length);
+                }
+                SpawnGem(gems[gemIndex], i, j);
             }
         }
+    }
+
+    private void Update()
+    {
+        matchFind.FindAllMatches();
     }
 
     private void SpawnGem(Gem g, int x, int y)
     {
         Gem gem = Instantiate(g, new Vector3(x, y, 0), Quaternion.identity);
         gem.transform.parent = transform;
-        gemLayout[x, y] = gem;
+        allGems[x, y] = gem;
         gem.SetupGem(x, y, this);
+    }
+
+    bool MatchesAt(Vector2Int posToCheck, Gem gemtoCheck)
+    {
+        if (posToCheck.x > 1)
+        {
+            if (allGems[posToCheck.x - 1, posToCheck.y].type == gemtoCheck.type && allGems[posToCheck.x - 2, posToCheck.y].type == gemtoCheck.type)
+                return true;
+        }
+        if (posToCheck.y > 1)
+        {
+            if (allGems[posToCheck.x, posToCheck.y - 1].type == gemtoCheck.type && allGems[posToCheck.x, posToCheck.y - 2].type == gemtoCheck.type)
+                return true;
+        }
+        return false;
+    }
+    
+    private void DestroyAt(Vector2Int pos)
+    {
+        if (allGems[pos.x,pos.y] != null)
+        {
+            Destroy(allGems[pos.x, pos.y].gameObject);
+            allGems[pos.x, pos.y] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        foreach (Gem g in matchFind.matched)
+        {
+            if (g != null)
+                DestroyAt(g.position);
+        }
+        StartCoroutine(MoveGemsDown());
+    }
+    private IEnumerator MoveGemsDown()
+    {
+        yield return new WaitForSeconds(.2f);
+        for (int x = 0; x < width; x++)
+        {
+            int gaps = 0;
+            for (int y = 0; y < height; y++)
+            {
+                if (allGems[x, y] == null)
+                    gaps++;
+                else if (gaps > 0)
+                {
+                    allGems[x, y].position.y -= gaps;
+                    allGems[x, y - gaps] = allGems[x, y];
+                    allGems[x, y] = null;
+                }
+            }
+        }
+        StartCoroutine(FillBoard());
+    }
+
+    private IEnumerator FillBoard()
+    {
+        yield return new WaitForSeconds(.5f);
+        RefillBoard();
+    }
+
+    private void RefillBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (allGems[x, y] == null)
+                {
+                    int gemIndex = Random.Range(0, gems.Length);
+                    SpawnGem(gems[gemIndex], x, y);
+                }
+            }
+        }
     }
 }
--- Match3/Assets/Scripts/Gem.cs
+++ Match3/Assets/Scripts/Gem.cs
@@ -8,6 +8,24 @@ public class Gem : MonoBehaviour
     public Vector2Int position;
     [HideInInspector]
     public Board board;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    private bool mousePressed;
+    private float swipeAngle = 0f;
+    private Gem otherGem;
+    public enum GemType
+    {
+        Blue,
+        Green,
+        Red,
+        Yellow,
+        Purple
+    }
+    public GemType type;
+    [HideInInspector]
+    public bool isMatched;
+    [HideInInspector]
+    public Vector2Int previousPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +35,22 @@ public class Gem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Vector2.Distance(transform.position, position) > .01f)
+        {
+            transform.position = Vector2.Lerp(transform.position, position, board.gemSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector3(position.x, position.y, 0);
+            board.allGems[position.x, position.y] = this;
+        }
+            
+        if (mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
         
     }
 
@@ -24,5 +58,75 @@ public class Gem : MonoBehaviour
     {
         position = new Vector2Int(x, y);
         board = b;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+    
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        swipeAngle = swipeAngle * 180 / Mathf.PI;
+        if (Vector3.Distance(firstTouchPosition,finalTouchPosition) > .5f)
+        {
+            MovePieces();
+        }
+    }
+
+    private void MovePieces()
+    {
+        previousPosition = position;
+        if (swipeAngle < 45 && swipeAngle > -45 && position.x < board.width - 1)
+        {
+            otherGem = board.allGems[position.x + 1, position.y];
+            otherGem.position.x--;
+            position.x++;
+        }
+        else if (swipeAngle <= 135 && swipeAngle > 45 && position.y < board.height - 1)
+        {
+            otherGem = board.allGems[position.x, position.y + 1];
+            otherGem.position.y--;
+            position.y++;
+        }
+        else if (swipeAngle >= -135 && swipeAngle < -45 && position.y > 0)
+        {
+            otherGem = board.allGems[position.x, position.y - 1];
+            otherGem.position.y++;
+            position.y--;
+        }
+        else if ((swipeAngle > 135 || swipeAngle < -135) && position.x > 0)
+        {
+            otherGem = board.allGems[position.x - 1, position.y];
+            otherGem.position.x++;
+            position.x--;
+        }
+        board.allGems[position.x, position.y] = this;
+        board.allGems[otherGem.position.x, otherGem.position.y] = otherGem;
+
+        StartCoroutine(CheckMove());
+    }
+
+    public IEnumerator CheckMove()
+    {
+        yield return new WaitForSeconds(.5f);
+        board.matchFind.FindAllMatches();
+        if (otherGem != null)
+        {
+            if (!isMatched && !otherGem.isMatched)
+            {
+                otherGem.position = position;
+                position = previousPosition;
+
+                board.allGems[position.x, position.y] = this;
+                board.allGems[otherGem.position.x, otherGem.position.y] = otherGem;
+            }
+            else
+            {
+                board.DestroyMatches();
+            }
+        }
     }
 }
